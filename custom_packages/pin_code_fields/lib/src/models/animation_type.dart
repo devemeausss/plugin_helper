2924d7c9ea80688245ee97f70cce52dfb40e86d8
dev au <tharo@devx.com.au>
2024-01-10 14:18:03 +0700

    Optimise pin_code_fields

--- custom_packages/pin_code_fields/lib/src/models/animation_type.dart
+++
@@ -1,3 +0,0 @@
-part of pin_code_fields;
-
-enum AnimationType { scale, slide, fade, none }
--- custom_packages/pin_code_fields/lib/src/models/haptic_feedback_type.dart
+++
@@ -1,9 +0,0 @@
-part of pin_code_fields;
-
-enum HapticFeedbackTypes {
-  heavy,
-  light,
-  medium,
-  selection,
-  vibrate,
-}
