899595c6437c49875dd0befbeabcda4e0ed9af8e
dev au <>
2024-04-16 20:45:03 +0700

    Customise video player

--- lib/widgets/widget_video_playing.dart
+++ lib/widgets/widget_video_playing.dart
@@ -31,7 +31,7 @@ class WidgetVideoPlaying extends StatefulWidget {
 }
 
 class _WidgetVideoPlayingState extends State<WidgetVideoPlaying> {
-  bool _isControl = false, _isLoading = false;
+  bool _isControl = false;
 
   @override
   void initState() {
@@ -61,152 +61,159 @@ class _WidgetVideoPlayingState extends State<WidgetVideoPlaying> {
 
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
-      child: GestureDetector(
-        onTap: () {
-          if (!mounted) return;
-          setState(() {
-            _isControl = !_isControl;
-          });
-        },
-        child: Container(
-          color: Colors.black,
-          width: double.infinity,
-          height: double.infinity,
-          child: Stack(
-            children: [
-              ValueListenableBuilder(
-                  valueListenable: widget.controller,
-                  builder: (context, VideoPlayerValue value, child) {
-                    if (value.isInitialized) {
-                      return SizedBox.expand(
-                          child: FittedBox(
-                        fit: BoxFit.contain,
-                        child: SizedBox(
-                          width: widget.controller.value.size.width,
-                          height: widget.controller.value.size.height,
-                          child: VideoPlayer(widget.controller),
-                        ),
-                      ));
-                    }
-                    return widget.loader;
-                  }),
-              ValueListenableBuilder(
-                  valueListenable: widget.controller,
-                  builder: (context, VideoPlayerValue value, child) {
-                    double widthPosition = value.position.inHours > 0 ? 55 : 40;
-                    double widthDuration = value.duration.inHours > 0 ? 55 : 40;
-                    double paddingBottom = widget.safeViewBottom;
-                    return Positioned(
-                        child: Align(
-                            alignment: Alignment.bottomCenter,
-                            child: AnimatedOpacity(
-                              opacity: _isControl ? 1 : 0,
-                              duration: const Duration(milliseconds: 200),
-                              child: Padding(
-                                padding: EdgeInsets.only(
-                                    bottom: paddingBottom, left: 10, right: 10),
-                                child: Container(
-                                  width: double.infinity,
-                                  height: 60,
-                                  decoration: BoxDecoration(
-                                      color: Colors.grey.withOpacity(0.3),
-                                      borderRadius: BorderRadius.circular(10)),
-                                  alignment: Alignment.centerLeft,
-                                  child: Row(
-                                    children: [
-                                      IconControl(
-                                        onPress: () async {
-                                          if (!_isControl) {
-                                            return;
-                                          }
-                                          if (value.isPlaying) {
-                                            await widget.controller.pause();
-                                          } else {
-                                            widget.controller.play();
-                                          }
-                                        },
-                                        icon: value.isPlaying
-                                            ? (widget.pauseIcon ??
-                                                const Icon(
-                                                  Icons.pause,
-                                                  size: 30,
-                                                ))
-                                            : (widget.playIcon ??
-                                                const Icon(
-                                                  Icons.play_arrow_rounded,
-                                                  size: 30,
-                                                )),
-                                      ),
-                                      Expanded(
-                                          child: Row(
-                                        children: [
-                                          SizedBox(
-                                            width: widthPosition,
-                                            child: Text(
-                                              value.position.videoDuration,
-                                              style: widget.positionStyle,
-                                            ),
-                                          ),
-                                          8.w,
-                                          Expanded(
-                                            child: ProgressBar(
-                                              progress: value.position,
-                                              buffered: Duration.zero,
-                                              total: value.duration,
-                                              progressBarColor:
-                                                  widget.progressBarColor,
-                                              baseBarColor: Colors.white
-                                                  .withOpacity(0.24),
-                                              bufferedBarColor: Colors.white
-                                                  .withOpacity(0.24),
-                                              thumbColor: Colors.white,
-                                              barHeight: 3.0,
-                                              thumbRadius: 5.0,
-                                              timeLabelLocation:
-                                                  TimeLabelLocation.none,
-                                              onSeek: (duration) {
-                                                widget.controller
-                                                    .seekTo(duration);
-                                              },
-                                            ),
-                                          ),
-                                          8.w,
-                                          SizedBox(
-                                            width: widthDuration,
-                                            child: Text(
-                                              value.duration.videoDuration,
-                                              style: widget.durationStyle,
-                                            ),
-                                          ),
-                                        ],
-                                      )),
-                                      IconControl(
-                                        onPress: () async {
-                                          if (!_isControl || _isLoading) return;
-                                          _isLoading = true;
-                                          if (widget
-                                              .controller.value.isPlaying) {
-                                            try {
+    return PopScope(
+      onPopInvoked: (didPop) async {
+        if (widget.controller.value.isPlaying) {
+          try {
+            await widget.controller.pause();
+          } catch (e) {}
+        }
+      },
+      child: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+        child: GestureDetector(
+          onTap: () {
+            if (!mounted) return;
+            setState(() {
+              _isControl = !_isControl;
+            });
+          },
+          child: Container(
+            color: Colors.black,
+            width: double.infinity,
+            height: double.infinity,
+            child: Stack(
+              children: [
+                ValueListenableBuilder(
+                    valueListenable: widget.controller,
+                    builder: (context, VideoPlayerValue value, child) {
+                      if (value.isInitialized) {
+                        return SizedBox.expand(
+                            child: FittedBox(
+                          fit: BoxFit.contain,
+                          child: SizedBox(
+                            width: widget.controller.value.size.width,
+                            height: widget.controller.value.size.height,
+                            child: VideoPlayer(widget.controller),
+                          ),
+                        ));
+                      }
+                      return widget.loader;
+                    }),
+                ValueListenableBuilder(
+                    valueListenable: widget.controller,
+                    builder: (context, VideoPlayerValue value, child) {
+                      double widthPosition =
+                          value.position.inHours > 0 ? 55 : 40;
+                      double widthDuration =
+                          value.duration.inHours > 0 ? 55 : 40;
+                      double paddingBottom = widget.safeViewBottom;
+                      return Positioned(
+                          child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: AnimatedOpacity(
+                                opacity: _isControl ? 1 : 0,
+                                duration: const Duration(milliseconds: 200),
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      bottom: paddingBottom,
+                                      left: 10,
+                                      right: 10),
+                                  child: Container(
+                                    width: double.infinity,
+                                    height: 60,
+                                    decoration: BoxDecoration(
+                                        color: Colors.grey.withOpacity(0.3),
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    alignment: Alignment.centerLeft,
+                                    child: Row(
+                                      children: [
+                                        IconControl(
+                                          onPress: () async {
+                                            if (!_isControl) {
+                                              return;
+                                            }
+                                            if (value.isPlaying) {
                                               await widget.controller.pause();
-                                            } catch (e) {}
-                                          }
-                                          goBack();
-                                        },
-                                        icon: widget.closeIcon ??
-                                            const Icon(
-                                              Icons.close,
-                                              size: 20,
+                                            } else {
+                                              widget.controller.play();
+                                            }
+                                          },
+                                          icon: value.isPlaying
+                                              ? (widget.pauseIcon ??
+                                                  const Icon(
+                                                    Icons.pause,
+                                                    size: 30,
+                                                  ))
+                                              : (widget.playIcon ??
+                                                  const Icon(
+                                                    Icons.play_arrow_rounded,
+                                                    size: 30,
+                                                  )),
+                                        ),
+                                        Expanded(
+                                            child: Row(
+                                          children: [
+                                            SizedBox(
+                                              width: widthPosition,
+                                              child: Text(
+                                                value.position.videoDuration,
+                                                style: widget.positionStyle,
+                                              ),
                                             ),
-                                      )
-                                    ],
+                                            8.w,
+                                            Expanded(
+                                              child: ProgressBar(
+                                                progress: value.position,
+                                                buffered: Duration.zero,
+                                                total: value.duration,
+                                                progressBarColor:
+                                                    widget.progressBarColor,
+                                                baseBarColor: Colors.white
+                                                    .withOpacity(0.24),
+                                                bufferedBarColor: Colors.white
+                                                    .withOpacity(0.24),
+                                                thumbColor: Colors.white,
+                                                barHeight: 3.0,
+                                                thumbRadius: 5.0,
+                                                timeLabelLocation:
+                                                    TimeLabelLocation.none,
+                                                onSeek: (duration) {
+                                                  widget.controller
+                                                      .seekTo(duration);
+                                                },
+                                              ),
+                                            ),
+                                            8.w,
+                                            SizedBox(
+                                              width: widthDuration,
+                                              child: Text(
+                                                value.duration.videoDuration,
+                                                style: widget.durationStyle,
+                                              ),
+                                            ),
+                                          ],
+                                        )),
+                                        IconControl(
+                                          onPress: () async {
+                                            if (!_isControl) return;
+                                            Navigator.pop(context);
+                                          },
+                                          icon: widget.closeIcon ??
+                                              const Icon(
+                                                Icons.close,
+                                                size: 20,
+                                              ),
+                                        )
+                                      ],
+                                    ),
                                   ),
                                 ),
-                              ),
-                            )));
-                  })
-            ],
+                              )));
+                    })
+              ],
+            ),
           ),
         ),
       ),
