f2c8d6df7f831a44333f2ff2e7874196bc562b06
dev au <>
2025-03-17 15:34:22 +0700

    Change path

--- lib/config/app_assets.dart
+++ lib/config/app_assets.dart
@@ -1,6 +1,6 @@
 class MyPluginAppAssets {
   final String visa = 'assets/icons/visa.png';
   final String amex = 'assets/icons/amex.png';
-  final String discovery = 'assets/icons/discovery.png';
+  final String discover = 'assets/icons/discover.png';
   final String mastercard = 'assets/icons/mastercard.png';
 }
